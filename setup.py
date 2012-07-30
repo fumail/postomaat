778386595be2505590fde4e469fec7962682d679
gryphius <oli@fuglu.org>
2012-07-30 15:56:38 +0200

    versioning system from fuglu

--- setup.py
+++ setup.py
@@ -2,10 +2,38 @@ from distutils.core import setup
 import glob
 import sys
 sys.path.insert(0,'src')
-from postomaat import POSTOMAAT_VERSION
+
+
+#store old content of version file here
+#if we have git available, temporarily overwrite the file
+#so we can report the git commit id in fuglu --version 
+OLD_VERSFILE_CONTENT=None
+VERSFILE='src/postomaat/__init__.py'
+
+def git_version():
+    from postomaat import POSTOMAAT_VERSION
+    global VERSFILE,OLD_VERSFILE_CONTENT
+    try:
+        import subprocess
+        x=subprocess.Popen(['git','describe'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ret=x.wait()
+        if ret==0:
+            stdout,stderr=x.communicate()
+            vers=stdout.strip()
+            if os.path.isfile(VERSFILE):
+                OLD_VERSFILE_CONTENT=open(VERSFILE,'r').read()
+                buff=OLD_VERSFILE_CONTENT.replace(POSTOMAAT_VERSION,vers)
+                open(VERSFILE,'w').write(buff)
+            return vers
+        else:
+            return POSTOMAAT_VERSION
+    except Exception,e:
+        return POSTOMAAT_VERSION
+
+
 
 setup(name = "postomaat",
-    version = POSTOMAAT_VERSION,
+    version = git_version(),
     description = "Postomaat Policy Daemon",
     author = "O. Schacher",
     url='http://www.wgwh.ch',
