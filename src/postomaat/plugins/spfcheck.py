4613fc9be2b36f7c1a58f66341e1288c048d78d9
gryphius <oli@fuglu.org>
2014-09-15 13:55:10 +0200

    rewrite spf checker tool with auto file reloader, drop domain whitelist, add selective spf domain list

--- src/postomaat/plugins/spfcheck.py
+++ src/postomaat/plugins/spfcheck.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-from postomaat.shared import ScannerPlugin, DUNNO, REJECT, DEFER_IF_PERMIT, strip_address, extract_domain
+from postomaat.shared import ScannerPlugin, DUNNO, strip_address, extract_domain, apply_template
+from postomaat.filetools import ListConfigFile
 try:
     import spf
     have_spf = True
@@ -13,99 +14,132 @@ try:
 except:
     have_netaddr = False
     
-    
-    
+
 class SPFPlugin(ScannerPlugin):
+    """This plugin performs SPF validation using the pyspf module https://pypi.python.org/pypi/pyspf/
+    by default, it just logs the result (test mode)
+
+    to enable actual rejection of messages, add a config option on_<resulttype> with a valid postfix action. eg:
+
+    on_fail = REJECT
+
+    valid result types are: 'pass', 'permerror', 'fail', 'temperror', 'softfail', 'none', and 'neutral'
+    """
                                          
     def __init__(self,config,section=None):
         ScannerPlugin.__init__(self,config,section)
         self.logger=self._logger()
         
         self.requiredvars={
-            'ip_whitelist':{
-                'default':'127.0.0.0/8',
-                'description':'which sending hosts/networks should be excluded from SPF checks',
-            },
-            'domain_whitelist':{
+            'ip_whitelist_file':{
                 'default':'',
-                'description':'which sender domains should be excluded from SPF checks',
+                'description':'file containing a list of ip adresses to be exempted from SPF checks. Supports CIDR notation if the netaddr module is installed. 127.0.0.0/8 is always exempted',
+            },
+            'domain_selective_spf_file':{
+                'default':'',
+                'description':'if this is non-empty, only sender domains in this file will be checked for SPF',
+            },
+            'on_fail':{
+                'default':'DUNNO',
+                'description':'Action for SPF fail.',
             },
             'on_softfail':{
                 'default':'DUNNO',
-                'description':'what to do on softfail',
+                'description':'Action for SPF softfail.',
             },
-            'on_softerror':{
-                'default':'DEFER',
-                'description':'what to do on softerror',
-            },
+            'messagetemplate':{
+                'default':'SPF ${result} for domain ${from_domain} from ${client_address} : ${explanation}'
+            }
         }
         
-        
-        
+        self.ip_whitelist_loader=None
+        self.ip_whitelist=[] # either a list of plain ip adress strings or a list of IPNetwork if netaddr is available
+
+        self.selective_domain_loader=None
+
+
+    def is_private_address(self,addr):
+        if addr=='127.0.0.1' or addr=='::1' or addr.startswith('10.') or addr.startswith('192.168.') or addr.startswith('fe80:'):
+            return True
+        if not addr.startswith('172.'):
+            return False
+        for i in range(16,32):
+            if addr.startswith('172.'+i):
+                return True
+        return False
+
+
+    def ip_whitelisted(self,addr):
+        if self.is_private_address(addr):
+            return DUNNO
+
+        #check ip whitelist
+        ip_whitelist_file=self.config.get(self.section,'ip_whitelist_file')
+        if ip_whitelist_file!=None:
+            if self.ip_whitelist_loader==None:
+                self.ip_whitelist_loader=ListConfigFile(ip_whitelist_file,lowercase=True)
+
+            if self.ip_whitelist_loader.file_changed():
+                plainlist=self.ip_whitelist_loader.get_content()
+
+                if have_netaddr:
+                    self.ip_whitelist=[IPNetwork(x) for x in plainlist]
+                else:
+                    self.ip_whitelist=plainlist
+            if have_netaddr:
+                checkaddr=IPAddress(addr)
+                for net in self.ip_whitelist:
+                        if checkaddr in net:
+                            return True
+            else:
+                if addr in plainlist:
+                        return True
+        return False
+
+
     def examine(self,suspect):
         if not have_spf:
             return DUNNO
         
         client_address=suspect.get_value('client_address')
-        if client_address is None:
-            self.logger.info('No client address found')
-            return DUNNO
-        if have_netaddr:
-            ip_whitelist=self.config.get('SPFPlugin','ip_whitelist')
-            ip_whitelist=[IPNetwork(i.strip()) for i in ip_whitelist.split(',')]
-            for net in ip_whitelist:
-                if IPAddress(client_address) in net:
-                    return DUNNO
-        
-        sender=suspect.get_value('sender')
-        if sender is None:
-            self.logger.warning('No RCPT address found')
-            return DEFER_IF_PERMIT,'internal policy error (no from address)'
-        sender_email = strip_address(sender)
-        try:
-            sender_domain = extract_domain(sender_email)
-        except ValueError as e:
-            self.logger.warning(str(e))
-            return DUNNO
-            
-        domain_whitelist=self.config.get('SPFPlugin','domain_whitelist')
-        domain_whitelist=[i.strip() for i in domain_whitelist.split(',')]
-        if sender_domain in domain_whitelist:
-            return DUNNO
-        
         helo_name=suspect.get_value('helo_name')
-        if helo_name is None:
-            self.logger.error('No SMTP HELO name found')
+        sender=suspect.get_value('sender')
+        if client_address is None or helo_name is None or sender is None:
+            self.logger.error('missing client_address or helo or sender')
             return DUNNO
-        
-        on_softfail=self.config.get('SPFPlugin','on_softfail')
-        softfail = DUNNO
-        if on_softfail == 'DEFER':
-            softfail =  DEFER_IF_PERMIT
-        on_softerror=self.config.get('SPFPlugin','on_softerror')
-        softerror = DUNNO
-        if on_softerror == 'REJECT':
-            softerror = REJECT
-        
+
+        if self.ip_whitelisted(client_address):
+            self.logger.info("Client %s is whitelisted - no SPF check"%client_address)
+            return DUNNO
+
+        sender_email = strip_address(sender)
+
+        selective_sender_domain_file=self.config.get(self.section,'domain_selective_spf_file')
+        if selective_sender_domain_file!='':
+            if self.selective_domain_loader==None:
+                self.selective_domain_loader=ListConfigFile(selective_sender_domain_file,lowercase=True)
+            try:
+                sender_domain = extract_domain(sender_email)
+            except ValueError as e:
+                self.logger.warning(str(e))
+                return DUNNO
+            if not sender_domain.lower() in self.selective_domain_loader.get_content():
+                return DUNNO
+
         result, explanation = spf.check2(client_address, sender_email, helo_name)
-        self.logger.debug('Postomaat SPF check: ip: %s, from: %s, helo: %s, result: %s' % (client_address, sender_email, helo_name, result))
+        suspect.tags['spf']=result
+        if result!='none':
+            self.logger.info('SPF client=%s, sender=%s, h=%s result=%s : %s' % (client_address, sender_email, helo_name, result,explanation))
         
         action = DUNNO
-        message = None
-        
-        if result == 'fail':
-            action = REJECT
-            message = explanation
-        elif result == 'temperror':
-            action = DEFER_IF_PERMIT
-            message = explanation
-        elif result == 'softfail' and softfail != DUNNO:
-            action = softfail
-            message = explanation
-        elif result == 'softerror' and softerror != DUNNO:
-            action = softerror
-            message = explanation
-        
+        message = apply_template(self.config.get(self.section,'messagetemplate'),suspect,dict(result=result,explanation=explanation))
+
+        configopt='on_%s'%result
+        if self.config.has_option(self.section,configopt):
+            action=self.config.get(self.section,configopt)
+        else:
+            action=DUNNO
+
         return action, message
          
         
@@ -118,12 +152,13 @@ class SPFPlugin(ScannerPlugin):
             lint_ok = False
             
         if not have_netaddr:
-            print 'netaddr python module not installed - IP whitelist is disabled'
-            lint_ok = False
-        
+            print 'WARNING: netaddr python module not installed - IP whitelist will not support CIDR notation'
+
         if not self.checkConfig():
             print 'Error checking config'
             lint_ok = False
         
         return lint_ok
-        
+
+    def __str__(self):
+        return "SPF"
