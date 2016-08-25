db77be956b2543df9eb7e06b3f12cfe16c360a11
neonknight <github@neonknight.ch>
2016-08-25 14:35:34 +0200

    update db.py with code from fuglu

--- src/postomaat/db.py
+++ src/postomaat/db.py
@@ -1,24 +1,34 @@
+# -*- coding: UTF-8 -*-
 SQLALCHEMY_AVAILABLE=False
 try:
     from sqlalchemy import create_engine
     from sqlalchemy.orm import scoped_session, sessionmaker
     SQLALCHEMY_AVAILABLE=True
-except:
+except ImportError:
     pass
 
-_enginecache={}
 
-def get_session(dburl):
-    if dburl in _enginecache:
-        engine= _enginecache[dburl]
+_sessmaker = None
+_engines = {}
+
+
+def get_session(connectstring, **kwargs):
+    global SQLALCHEMY_AVAILABLE
+    global _sessmaker
+    global _engines
+
+    if not SQLALCHEMY_AVAILABLE:
+        raise Exception("sql extension not enabled")
+
+    if connectstring in _engines:
+        engine = _engines[connectstring]
     else:
-        engine = create_engine(dburl,pool_size=20,pool_recycle=300)
-    _enginecache[dburl]=engine
-    maker = sessionmaker(autoflush=True, autocommit=True)
-    session = scoped_session(maker)
+        engine = create_engine(connectstring, pool_recycle=20)
+        _engines[connectstring] = engine
+
+    if _sessmaker == None:
+        _sessmaker = sessionmaker(autoflush=True, autocommit=True, **kwargs)
+
+    session = scoped_session(_sessmaker)
     session.configure(bind=engine)
     return session
-
-
-def get_connection(dburl):
-    return get_session(dburl)    
