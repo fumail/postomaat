053e686826933e8f5af243bfc8883e7240174de4
neonknight <github@neonknight.ch>
2017-07-03 07:54:23 +0200

    db based spf check config

--- src/postomaat/plugins/spfcheck.py
+++ src/postomaat/plugins/spfcheck.py
@@ -1,17 +1,25 @@
 # -*- coding: UTF-8 -*-
 
-from postomaat.shared import ScannerPlugin, DUNNO, strip_address, extract_domain, apply_template, FileList, string_to_actioncode
+from postomaat.shared import ScannerPlugin, DUNNO, strip_address, extract_domain, apply_template, \
+    FileList, string_to_actioncode, SettingsCache
+from postomaat.db import SQLALCHEMY_AVAILABLE, get_session, get_domain_setting
+import os
 try:
     import spf
-    have_spf = True
+    HAVE_SPF = True
 except ImportError:
-    have_spf = False
+    HAVE_SPF = False
     
 try:
     from netaddr import IPAddress, IPNetwork
-    have_netaddr = True
+    HAVE_NETADDR = True
 except ImportError:
-    have_netaddr = False
+    HAVE_NETADDR = False
+
+
+
+SETTINGSCACHE=None
+
     
 
 class SPFPlugin(ScannerPlugin):
@@ -38,6 +46,14 @@ class SPFPlugin(ScannerPlugin):
                 'default':'',
                 'description':'if this is non-empty, only sender domains in this file will be checked for SPF',
             },
+            'dbconnection':{
+                'default':"mysql://root@localhost/spfcheck?charset=utf8",
+                'description':'SQLAlchemy Connection string. Leave empty to disable SQL lookups',
+            },
+            'domain_sql_query':{
+                'default':"SELECT check_spf from domain where domain_name=:domain",
+                'description':'get from sql database :domain will be replaced with the actual domain name. must return field check_spf',
+            },
             'on_fail':{
                 'default':'DUNNO',
                 'description':'Action for SPF fail.',
@@ -55,6 +71,32 @@ class SPFPlugin(ScannerPlugin):
         self.ip_whitelist=[] # either a list of plain ip adress strings or a list of IPNetwork if netaddr is available
 
         self.selective_domain_loader=None
+    
+    
+    def check_this_domain(self, from_domain):
+        global SETTINGSCACHE
+        
+        do_check = False
+        selective_sender_domain_file=self.config.get(self.section,'domain_selective_spf_file','').strip()
+        if selective_sender_domain_file != '' and os.path.exists(selective_sender_domain_file):
+            if self.selective_domain_loader is None:
+                self.selective_domain_loader=FileList(selective_sender_domain_file,lowercase=True)
+            if from_domain.lower() in self.selective_domain_loader.get_list():
+                do_check = True
+                
+        if not do_check:
+            dbconnection = self.config.get(self.section, 'dbconnection', '').strip()
+            sqlquery = self.config.get(self.section, 'domain_sql_query')
+            
+            if dbconnection!='' and SQLALCHEMY_AVAILABLE:
+                if SETTINGSCACHE is None:
+                    SETTINGSCACHE = SettingsCache()
+                do_check = get_domain_setting(from_domain, dbconnection, sqlquery, SETTINGSCACHE, False, self.logger)
+                
+            elif dbconnection!='' and not SQLALCHEMY_AVAILABLE:
+                self.logger.error('dbconnection specified but sqlalchemy not available - skipping db lookup')
+                
+        return do_check
 
 
     def is_private_address(self,addr):
@@ -73,8 +115,8 @@ class SPFPlugin(ScannerPlugin):
             return True
 
         #check ip whitelist
-        ip_whitelist_file=self.config.get(self.section,'ip_whitelist_file')
-        if ip_whitelist_file is not None:
+        ip_whitelist_file=self.config.get(self.section,'ip_whitelist_file', '').strip()
+        if ip_whitelist_file != '' and os.path.exists(ip_whitelist_file):
             plainlist = []
             if self.ip_whitelist_loader is None:
                 self.ip_whitelist_loader=FileList(ip_whitelist_file,lowercase=True)
@@ -82,12 +124,12 @@ class SPFPlugin(ScannerPlugin):
             if self.ip_whitelist_loader.file_changed():
                 plainlist=self.ip_whitelist_loader.get_list()
 
-                if have_netaddr:
+                if HAVE_NETADDR:
                     self.ip_whitelist=[IPNetwork(x) for x in plainlist]
                 else:
                     self.ip_whitelist=plainlist
 
-            if have_netaddr:
+            if HAVE_NETADDR:
                 checkaddr=IPAddress(addr)
                 for net in self.ip_whitelist:
                     if checkaddr in net:
@@ -99,7 +141,7 @@ class SPFPlugin(ScannerPlugin):
 
 
     def examine(self,suspect):
-        if not have_spf:
+        if not HAVE_SPF:
             return DUNNO
         
         client_address=suspect.get_value('client_address')
@@ -116,32 +158,27 @@ class SPFPlugin(ScannerPlugin):
         sender_email = strip_address(sender)
         if sender_email=='' or sender_email is None:
             return DUNNO
-
-        selective_sender_domain_file=self.config.get(self.section,'domain_selective_spf_file')
-        if selective_sender_domain_file!='':
-            if self.selective_domain_loader is None:
-                self.selective_domain_loader=FileList(selective_sender_domain_file,lowercase=True)
-            try:
-                sender_domain = extract_domain(sender_email)
-                if sender_domain is None:
-                    return DUNNO
-            except ValueError as e:
-                self.logger.warning(str(e))
-                return DUNNO
-            if not sender_domain.lower() in self.selective_domain_loader.get_list():
-                return DUNNO
+        
+        sender_domain = extract_domain(sender_email)
+        if sender_domain is None:
+            self.logger.error('no domain found in sender address %s' % sender_email)
+            return DUNNO
+        
+        if not self.check_this_domain(sender_domain):
+            self.logger.debug('skipping SPF check for %s' % sender_domain)
+            return DUNNO
 
         result, explanation = spf.check2(client_address, sender_email, helo_name)
-        suspect.tags['spf']=result
-        if result!='none':
+        suspect.tags['spf'] = result
+        if result != 'none':
             self.logger.info('SPF client=%s, sender=%s, h=%s result=%s : %s' % (client_address, sender_email, helo_name, result,explanation))
         
         action = DUNNO
-        message = apply_template(self.config.get(self.section,'messagetemplate'),suspect,dict(result=result,explanation=explanation))
+        message = apply_template(self.config.get(self.section, 'messagetemplate'), suspect, dict(result=result, explanation=explanation))
 
-        configopt='on_%s'%result
-        if self.config.has_option(self.section,configopt):
-            action=string_to_actioncode(self.config.get(self.section,configopt))
+        configopt = 'on_%s' % result
+        if self.config.has_option(self.section, configopt):
+            action=string_to_actioncode(self.config.get(self.section, configopt))
 
         return action, message
          
@@ -150,16 +187,45 @@ class SPFPlugin(ScannerPlugin):
     def lint(self):
         lint_ok = True
         
-        if not have_spf:
+        if not HAVE_SPF:
             print 'pyspf or pydns module not installed - this plugin will do nothing'
             lint_ok = False
             
-        if not have_netaddr:
+        if not HAVE_NETADDR:
             print 'WARNING: netaddr python module not installed - IP whitelist will not support CIDR notation'
 
         if not self.checkConfig():
             print 'Error checking config'
             lint_ok = False
+            
+        selective_sender_domain_file=self.config.get(self.section,'domain_selective_spf_file','').strip()
+        if selective_sender_domain_file != '' and not os.path.exists(selective_sender_domain_file):
+            print "domain_selective_spf_file %s does not exist" % selective_sender_domain_file
+            lint_ok = False
+            
+        ip_whitelist_file=self.config.get(self.section,'ip_whitelist_file', '').strip()
+        if ip_whitelist_file != '' and os.path.exists(ip_whitelist_file):
+            print "ip_whitelist_file %s does not exist - IP whitelist is disabled" % ip_whitelist_file
+            lint_ok = False
+        
+        sqlquery = self.config.get(self.section, 'domain_sql_query')
+        dbconnection = self.config.get(self.section, 'dbconnection', '').strip()
+        if not SQLALCHEMY_AVAILABLE and dbconnection != '':
+            print 'SQLAlchemy not available, cannot use SQL backend'
+            lint_ok = False
+        elif dbconnection == '':
+            print 'No DB connection defined. Disabling SQL backend'
+        else:
+            if not sqlquery.lower().startswith('select '):
+                lint_ok = False
+                print 'SQL statement must be a SELECT query'
+            if lint_ok:
+                try:
+                    conn=get_session(dbconnection)
+                    conn.execute(sqlquery, {'domain':'example.com'})
+                except Exception as e:
+                    lint_ok = False
+                    print str(e)
         
         return lint_ok
     
