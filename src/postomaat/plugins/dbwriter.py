922fde42e1bd4e98e7f738b2a2b2938e1e5649c9
neonknight <github@neonknight.ch>
2018-04-26 03:02:31 +0200

    dbwriter: cleanup imports and add licence information

--- src/postomaat/plugins/dbwriter.py
+++ src/postomaat/plugins/dbwriter.py
@@ -1,5 +1,21 @@
-# -*- coding: utf-8 -*-
-from postomaat.shared import *
+# -*- coding: UTF-8 -*-
+#   Copyright 2012-2018 Oli Schacher
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
+from postomaat.shared import ScannerPlugin, strip_address, extract_domain
 from postomaat.extensions.sql import SQL_EXTENSION_ENABLED,get_session
 
 
